1f34e8e337652845bf5b58c4ae5fb6e964c6d627
Abhishek karmakar <Abhishek.karmakar10@gmail.com>
2019-09-01 00:08:06 +0530

    Adding master method

--- scr/hello.kt
+++ scr/hello.kt
@@ -1,3 +1,12 @@
 fun main(){
 println("Hello World")
+methodOne("Abhishek")
+}
+
+fun methodOne(name: String){
+  println("Name is %s" name)
+}
+
+fun MethodMaster(){
+  Println("this is a master method")
 }
